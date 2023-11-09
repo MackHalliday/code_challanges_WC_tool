bc44944060d1b315634179bf51d3660df49609d9
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-09 17:41:27 -0500

    Refactor __init__ , add ability to count_lines

--- wc_tool.py
+++ wc_tool.py
@@ -1,14 +1,30 @@
 import os
 
 
-class WCTool():
+class WCTool:
     def __init__(self, command):
         self.command = command
-
-    def run_command(self):
-        arg_option, file_name = self.parse_command()
-        if arg_option == "-c":
-            return os.path.getsize(file_name)
+        self.arg_option = None
+        self.file_name = None
 
     def parse_command(self):
-        return self.command.split()
+        arg_option, file_name = self.command.split()
+        self.arg_option = arg_option
+        self.file_name = file_name
+
+    def run_command(self):
+        self.parse_command()
+
+        if self.arg_option == "-c":
+            option_value = os.path.getsize(self.file_name)
+
+        if self.arg_option == "-l":
+            option_value = self.count_lines(self.file_name)
+
+        return f"{option_value} {self.file_name}"
+
+    def count_lines(self, file_name):
+        with open(self.file_name) as file:
+            lines = file.readlines()
+            total_lines = len(lines)
+        return total_lines
