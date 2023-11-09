68be9687a6fdf50ba52439000d9d144d34da6ddc
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-09 17:20:14 -0500

    Update test to assert file name in response, add new test for num of lines

--- test_wc_tool.py
+++ test_wc_tool.py
@@ -2,8 +2,14 @@ from wc_tool import WCTool
 
 
 class TestWCTool:
-    def test_can_return_number_of_bytes_file(self):
+    def test_can_return_number_of_bytes_in_file(self):
         command = "-c test.txt"
         num_of_bytes = WCTool(command).run_command()
 
-        assert num_of_bytes == 342190
+        assert num_of_bytes == "342190 test.txt"
+
+    def test_can_return_number_of_lines_in_file(self):
+        command = "-c test.txt"
+        num_of_lines = WCTool(command).run_command()
+
+        assert num_of_lines == "7145 test.txt"
