39cf1f8eba1191f5e5fda58f399a6e20710b8225
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-11-12 09:45:50 -0500

    Run test for command line - skipped other tests until proper command line can be determined

--- tests/test_ccwc.py
+++ tests/test_ccwc.py
@@ -1,41 +1,45 @@
+import subprocess
+import pytest
+
+
 class TestWCTool:
     def test_can_return_number_of_bytes_in_file(self):
-        command = "python3 ccwc/ccwc.py -c tests/test.txt"
-        result = ""
+        result_bytes = subprocess.check_output(
+            ["python3", "ccwc/ccwc.py", "-c", "tests/test.txt"], universal_newlines=True
+        )
+        assert result_bytes == "342190 tests/test.txt\n"
 
-        assert result == "342190 test.txt"
-
+    @pytest.mark.skip()
     def test_can_return_number_of_lines_in_file(self):
         pass
-        command = "-l test.txt"
         result = ""
 
         assert result == "7145 test.txt"
 
+    @pytest.mark.skip()
     def test_can_return_number_of_words_in_file(self):
         pass
-        command = "-w test.txt"
         result = ""
 
         assert result == "58164 test.txt"
 
+    @pytest.mark.skip()
     def test_can_return_number_of_characters_in_file(self):
         pass
-        command = "-m test.txt"
         result = ""
 
-        assert num_of_characters == "339292 test.txt"
+        assert result == "339292 test.txt"
 
+    @pytest.mark.skip()
     def test_can_return_bytes_line_word_as_default_option(self):
         pass
-        command = "test.txt"
         result = ""
 
         assert result == "7145  58164  342190 test.txt"
 
+    @pytest.mark.skip()
     def test_error_handling_for_invalid_input(self):
         pass
-        command = "-z test.txt"
         result = ""
 
         assert (
